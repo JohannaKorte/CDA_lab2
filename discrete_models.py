4be9fa3d3b789d9ed265cabb4a8b54ba6986a8ea
rolf <rvers@protonmail.com>
2018-06-04 21:54:15 +0200

    refactored code

--- discrete_models.py
+++ discrete_models.py
@@ -1,15 +1,10 @@
 import pandas as pd
-#import matplotlib as mpl
 import matplotlib.pyplot as plt
 from sklearn.decomposition import PCA
 import numpy as np
 
-data_train1 = pd.read_csv("./Data/BATADAL_train1.csv")
-data_train2 = pd.read_csv("./Data/BATADAL_train2.csv")
-data_test = pd.read_csv("./Data/BATADAL_test.csv")
-data_train1['DATETIME'] = pd.to_datetime(data_train1['DATETIME'], format='%d/%m/%y %H')
-
-def discretize(data):
+def discretize(data): # DatetimeParsed -> DiscData
+    ''' Discretizes each of the columns into three bins. '''
     ndata = data.copy()
     for col in list(ndata):
         if col not in ['DATETIME', 'ATT_FLAG']:
@@ -18,22 +13,47 @@ def discretize(data):
     ndata = ndata.dropna(thresh=len(ndata) - 2, axis=1)
     return ndata
 
-def dummies(data):
-    return pd.get_dummies(data, columns=list(data))
+def dummies(data): # DiscData -> (DiscData, DummyData)
+    ''' Creates dummies for the dataset elements. '''
+    return data, pd.get_dummies(data, columns=list(data).remove('DATETIME'))
 
-def preprocess(data):
-    return dummies(discretize(data))
+def read_datetime(data): # RawData -> DatetimeParsed
+    ''' Properly parses the DATETIME field. '''
+    ndata = data.copy()
+    ndata['DATETIME'] = pd.to_datetime(ndata['DATETIME'], format='%d/%m/%y %H')
+    return ndata
 
-data_t1_disc = data_train1.drop(['DATETIME', 'ATT_FLAG'], axis=1) # datetime doesn't play nice with anything really
-data_t1_disc = discretize(data_t1_disc)
-data_t1_dummies = dummies(data_t1_disc)
+# data AnyData = RawData | DiscData | DummyData | DatetimeParsed
+def drop_columns(data, cols=['DATETIME', 'ATT_FLAG']): # AnyData -> NoDatetimeData
+    ''' Removes the defined columns. '''
+    return data.drop(cols, axis=1)
 
-pca = PCA()
-pca.fit(data_t1_dummies)
-transformed_t1 = pca.transform(data_t1_dummies) # map the dataset onto the PCA space
-sums_t1 = np.array([sum(x**2) for x in transformed_t1/pca.singular_values_]) # calc Chi Squared value elements
-anomalous = data_train1[sums_t1 > 0.25]
+def preprocess(data): # RawData -> (DiscData, DummyData)
+    return dummies(discretize(read_datetime(data)))
 
+# type AnomalyMarks = [Bool]
+def pca_detect(data): # DummyData -> Sums
+    ''' Uses PCA to find anomalies. '''
+    pca = PCA()
+    pca.fit(data)
+    transformed = pca.transform(data) # map the dataset onto the PCA space
+    return np.array([sum(x**2) for x in transformed/pca.singular_values_]) # calc Chi Squared value elements
+
+data_train1 = pd.read_csv("./Data/BATADAL_train1.csv")
+data_train2 = pd.read_csv("./Data/BATADAL_train2.csv", sep=', ')
+data_train2['ATT_FLAG'] = data_train2['ATT_FLAG'] == 1
+data_test = pd.read_csv("./Data/BATADAL_test.csv")
+
+data_t1_disc, data_t1_dummies = preprocess(data_train1)
+data_t2_disc, data_t2_dummies = preprocess(data_train2)
+
+# anomaly detection
+sums1 = pca_detect(drop_columns(data_t1_dummies))
+hits1 = (sums1>0.25) == data_t1_dummies['ATT_FLAG']
+sums2 = pca_detect(drop_columns(data_t2_dummies))
+hits2 = (sums2>0.025) == data_t2_dummies['ATT_FLAG']
+
+# plotting only 200 numbers
 data_train1 = data_train1[0:200]
 data_t1_disc = data_t1_disc[0:200]
 
